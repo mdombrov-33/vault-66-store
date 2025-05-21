b3d1cd815cf6d046d08a3d9abca41425959c653c
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-21 17:10:48 +0300

    create dropdown lins in navbar

---
+++ app/data/nav.ts
@@ -0,0 +1,10 @@
+import { NavLinks } from "../types/nav";
+
+export const navLinks: NavLinks[] = [
+  { href: "/", label: "home" },
+  { href: "/about", label: "about" },
+  { href: "/products", label: "products" },
+  { href: "/favorites", label: "favorites" },
+  { href: "/cart", label: "cart" },
+  { href: "/orders", label: "orders" },
+];
--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,5 +1,35 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { navLinks } from "@/app/data/nav";
+
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="w-6 h-6" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-48" align="start" sideOffset={10}>
+        {navLinks.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link className="capitalize" href={link.href}>
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
