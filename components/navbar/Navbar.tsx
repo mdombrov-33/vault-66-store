7613cd6ecde95675f5d3d54009f2b49e88f5e0c6
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-28 21:49:20 +0300

    add navsearch functionality

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,13 +4,19 @@ import DarkMode from "./DarkMode";
 import LinksDropdown from "./LinksDropdown";
 import Logo from "./Logo";
 import NavSearch from "./NavSearch";
+import { Suspense } from "react";
 
 function Navbar() {
   return (
     <nav className="border-b">
       <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4">
         <Logo />
-        <NavSearch />
+        {/* Use suspense here because of searchParams error.
+         If we use searchParams with static pages we won't be able to deploy project at all. 
+         https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
+        <Suspense>
+          <NavSearch />
+        </Suspense>
         <div className="flex gap-4 items-center">
           <CartBtn />
           <DarkMode />
