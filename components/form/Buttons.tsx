fc138d08a130e1a4fe9d3859ae5b3c4308507329
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-30 00:14:27 +0300

    commit some imports in Buttons to check deployment errors

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -4,11 +4,11 @@ import { ReloadIcon } from "@radix-ui/react-icons";
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { SignInButton } from "@clerk/nextjs";
-import { FaRegHeart, FaHeart } from "react-icons/fa";
-import { LuTrash2, LuPenSquare } from "react-icons/lu";
-import { ButtonSize } from "@/types/form";
 import { SubmitButtonProps } from "@/types/form";
+// import { SignInButton } from "@clerk/nextjs";
+// import { FaRegHeart, FaHeart } from "react-icons/fa";
+// import { LuTrash2, LuPenSquare } from "react-icons/lu";
+// import { ButtonSize } from "@/types/form";
 
 export function SubmitButton({
   className = "",
