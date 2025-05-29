f7d9dfcdafcfccd3dd69f95b4d8f766c1105a83d
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-29 20:03:30 +0300

    refactor create product: create separate FormInput component

---
+++ components/form/FormInput.tsx
@@ -0,0 +1,29 @@
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { FormInputProps } from "@/types/form";
+
+function FormInput({
+  name,
+  type,
+  label,
+  defaultValue,
+  placeholder,
+}: FormInputProps) {
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        {label || name}
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        type={type}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
+
+export default FormInput;
---
+++ types/form.ts
@@ -0,0 +1,7 @@
+export type FormInputProps = {
+  name: string;
+  type: string;
+  label?: string;
+  defaultValue?: string;
+  placeholder?: string;
+};
