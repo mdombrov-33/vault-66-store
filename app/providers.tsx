34f39d5cc0c0d2f527af347e1a4c959d43b52c88
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-29 08:33:57 +0300

    add toaster to providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 import React from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
