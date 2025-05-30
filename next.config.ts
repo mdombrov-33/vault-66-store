b1203d9fe2883a07264304b91bed64c6a2caaff5
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-30 19:28:40 +0300

    fix: tweak hostname name in next.config

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://opzuqgehxlasumgyysml.supabase.co",
+        hostname: "opzuqgehxlasumgyysml.supabase.co",
       },
     ],
   },
