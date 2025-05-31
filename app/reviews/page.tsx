45f959addb5e193124024fa3204d94010dfdef31
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-31 21:40:09 +0300

    feat: work on user reviews page

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,5 +1,35 @@
-function ReviewsPage() {
-  return <div>ReviewsPage</div>;
+import { deleteReviewAction, fetchProductReviewsByUser } from "@/utils/actions";
+import ReviewCard from "@/components/reviews/ReviewCard";
+import SectionTitle from "@/components/global/SectionTitle";
+import FormContainer from "@/components/form/FormContainer";
+import { IconButton } from "@/components/form/Buttons";
+
+async function ReviewsPage() {
+  const reviews = await fetchProductReviewsByUser();
+
+  if (!reviews) return <SectionTitle text="No reviews found" />;
+
+  return (
+    <>
+      <SectionTitle text="Your Reviews" />
+      <section className="grid md:grid-cols-2 gap-8 mt-4">
+        {reviews.map((review) => {
+          const { comment, rating } = review;
+          const { name, image } = review.product;
+          const reviewInfo = {
+            comment,
+            rating,
+            name,
+            image,
+          };
+
+          return (
+            <ReviewCard reviewInfo={reviewInfo} key={review.id}></ReviewCard>
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
 export default ReviewsPage;
