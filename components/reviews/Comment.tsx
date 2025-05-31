969c876bb9d611dcfc11174ce5b3a33da6ce1184
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-31 19:15:20 +0300

    feat: work on Rating and Comment components

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,5 +1,32 @@
-function Comment() {
-  return <div>Comment</div>;
-}
+"use Client";
 
+import { useState } from "react";
+import { Button } from "../ui/button";
+
+function Comment({ comment }: { comment: string }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  const longComment = comment.length > 100;
+  const displayComment =
+    longComment && !isExpanded ? `${comment.slice(0, 100)}...` : comment;
+
+  return (
+    <div>
+      <p className="text-sm">{displayComment}</p>
+      {longComment && (
+        <Button
+          className="text-muted-foreground pl-0"
+          onClick={toggleExpanded}
+          variant="link"
+        >
+          {isExpanded ? "Show less" : "Show more"}
+        </Button>
+      )}
+    </div>
+  );
+}
 export default Comment;
