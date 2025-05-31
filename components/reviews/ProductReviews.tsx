5a55c6fc156f91d3088acd8b455dcd3060cdf0c8
Maksym Dombrov <maksymdombrov@gmail.com>
2025-05-31 19:26:08 +0300

    feat: work on ProductReviews component

--- components/reviews/ProductReviews.tsx
+++ components/reviews/ProductReviews.tsx
@@ -1,5 +1,25 @@
-function ProductReviews() {
-  return <div>ProductReviews</div>;
+import { fetchProductReviews } from "@/utils/actions";
+import SectionTitle from "@/components/global/SectionTitle";
+import ReviewCard from "./ReviewCard";
+
+async function ProductReviews({ productId }: { productId: string }) {
+  const reviews = await fetchProductReviews(productId);
+
+  return (
+    <div className="mt-16">
+      <SectionTitle text="product reviews" />
+      {reviews.map((review) => {
+        const { comment, rating, authorImageUrl, authorName } = review;
+        const reviewInfo = {
+          comment,
+          rating,
+          image: authorImageUrl,
+          name: authorName,
+        };
+        return <ReviewCard key={review.id} reviewInfo={reviewInfo} />;
+      })}
+    </div>
+  );
 }
 
 export default ProductReviews;
