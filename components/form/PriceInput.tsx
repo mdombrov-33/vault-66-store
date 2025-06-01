0bb3e8ea03583963a86be2867496e99cbbd6850f
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-02 02:23:45 +0300

    fix currency in admin panel

--- components/form/PriceInput.tsx
+++ components/form/PriceInput.tsx
@@ -10,7 +10,7 @@ function PriceInput({ defaultValue }: FormInputNumberProps) {
   return (
     <div className="mb-2">
       <Label htmlFor={name} className="capitalize">
-        Price ($)
+        Price (Caps)
       </Label>
       <Input
         id={name}
