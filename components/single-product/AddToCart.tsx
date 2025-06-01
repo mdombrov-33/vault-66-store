5254c146ea0892ccef88e47ae40f6b53e9f13acc
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-01 18:07:14 +0300

    feat: work on AddToCart component

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,10 +1,34 @@
-import { Button } from "../ui/button";
+"use client";
+
+import { useState } from "react";
+import SelectProductAmount from "@/components/single-product/SelectProductAmount";
+import { Mode } from "@/types/enums";
+import FormContainer from "@/components/form/FormContainer";
+import { SubmitButton, ProductSignInButton } from "@/components/form/Buttons";
+import { addToCartAction } from "@/utils/actions";
+import { useAuth } from "@clerk/nextjs";
 
 function AddToCart({ productId }: { productId: string }) {
+  const [amount, setAmount] = useState(1);
+  const { userId } = useAuth();
+
   return (
-    <Button className="capitalize mt-8" size="lg">
-      add to cart
-    </Button>
+    <div className="mt-4">
+      <SelectProductAmount
+        mode={Mode.SingleProduct}
+        amount={amount}
+        setAmount={setAmount}
+      />
+      {userId ? (
+        <FormContainer action={addToCartAction}>
+          <input type="hidden" name="productId" value={productId} />
+          <input type="hidden" name="amount" value={amount} />
+          <SubmitButton text="add to cart" className="mt-8" />
+        </FormContainer>
+      ) : (
+        <ProductSignInButton />
+      )}
+    </div>
   );
 }
 
