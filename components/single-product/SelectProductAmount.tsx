257a832300779f7ebd5284fa6e844a9ffb8ccf63
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-01 16:40:04 +0300

    feat: finish SelectProductAmount component

--- components/single-product/SelectProductAmount.tsx
+++ components/single-product/SelectProductAmount.tsx
@@ -6,8 +6,40 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-function SelectProductAmount() {
-  return <div>SelectProductAmount</div>;
+import { SelectCartItemAmountProps } from "@/types/cart";
+import { Mode } from "@/types/enums";
+import { SelectProductAmountProps } from "@/types/product-ui";
+
+function SelectProductAmount(
+  props: SelectProductAmountProps | SelectCartItemAmountProps
+) {
+  const { mode, amount, setAmount } = props;
+  const cartItem = mode === Mode.CartItem;
+
+  return (
+    <>
+      <h4 className="mb-2">Amount: </h4>
+      <Select
+        defaultValue={amount.toString()}
+        onValueChange={(value) => setAmount(Number(value))}
+        disabled={cartItem ? props.isLoading : false}
+      >
+        <SelectTrigger className={cartItem ? "w-[100px]" : "w-[150px]"}>
+          <SelectValue placeholder={amount} />
+        </SelectTrigger>
+        <SelectContent>
+          {Array.from({ length: cartItem ? amount + 10 : 10 }, (_, i) => {
+            const selectValue = (i + 1).toString();
+            return (
+              <SelectItem key={selectValue} value={selectValue}>
+                {selectValue}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+    </>
+  );
 }
 
 export default SelectProductAmount;
