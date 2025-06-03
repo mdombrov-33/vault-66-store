7c316d062a4b644492479378af7d247155dd8d21
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-03 15:33:25 +0300

    refactor: refactor logic for createOrderAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -634,12 +634,24 @@ export const updateCartItemAction = async ({
 //* Creates a new order from the user's cart.
 export const createOrderAction = async (prevState: any, formData: FormData) => {
   const user = await getAuthUser();
+  let orderId: string | null = null;
+  let cartId: string | null = null;
 
   try {
     const cart = await fetchOrCreateCart({
       userId: user.id,
       errorOnFailure: true,
     });
+
+    cartId = cart.id;
+
+    await db.order.deleteMany({
+      where: {
+        clerkId: user.id,
+        isPaid: false,
+      },
+    });
+
     const order = await db.order.create({
       data: {
         clerkId: user.id,
@@ -651,15 +663,11 @@ export const createOrderAction = async (prevState: any, formData: FormData) => {
       },
     });
 
-    await db.cart.delete({
-      where: {
-        id: cart.id,
-      },
-    });
+    orderId = order.id;
   } catch (error) {
     return renderError(error);
   }
-  redirect("/orders");
+  redirect(`/checkout?orderId=${orderId}&cardId=${cartId}`);
 };
 
 //* Fetches all orders made by the authenticated user.
