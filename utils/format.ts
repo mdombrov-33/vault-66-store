c3d146ff8d9b31946a9a5c14addb76b0265f1017
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-03 13:28:06 +0300

    refactor: refactor format file in utils folder and create new formatDate helper

--- utils/format.ts
+++ utils/format.ts
@@ -1,9 +1,18 @@
 //* Helper function to format a number as USD currency.
 //* Defaults to $0.00 if amount is null or undefined.
+//* Not used in this app because it is thematically not relevant.
 export const formatCurrency = (amount: number | null) => {
   const value = amount || 0;
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   }).format(value);
+};
+
+export const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
 };
