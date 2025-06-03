98d8aa47d67f242b354ec54bd50e5fb8346d65fc
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-03 15:54:21 +0300

    feat: work on checkout page

---
+++ app/checkout/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import React, { useCallback } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+
+//* Flow: This page sends request to /api/payment => logic from /api/payment is send to stripe
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
+function CheckoutPage() {
+  const searchParams = useSearchParams();
+
+  //* From action createOrderAction
+  const orderId = searchParams.get("orderId");
+  const cartId = searchParams.get("cartId");
+
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post("/api/payment", {
+      orderId,
+      cartId,
+    });
+    return response.data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <section>
+      <div id="checkout">
+        <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+          <EmbeddedCheckout />
+        </EmbeddedCheckoutProvider>
+      </div>
+    </section>
+  );
+}
+
+export default CheckoutPage;
