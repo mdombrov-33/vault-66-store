a397b472d1cf89bd50decf3bdb9aee59eae70c34
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-03 17:39:36 +0300

    fix: try to fix deploy bug in checkout page

---
+++ app/checkout/CheckoutClient.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
+function CheckoutClient() {
+  const searchParams = useSearchParams();
+  const orderId = searchParams.get("orderId");
+  const cartId = searchParams.get("cartId");
+
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post("/api/payment", {
+      orderId,
+      cartId,
+    });
+    return response.data.clientSecret;
+  }, [orderId, cartId]);
+
+  const options = { fetchClientSecret };
+  return (
+    <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      <EmbeddedCheckout />
+    </EmbeddedCheckoutProvider>
+  );
+}
+
+export default CheckoutClient;
--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,41 +1,14 @@
 "use client";
-export const dynamic = "force-dynamic";
 
-import axios from "axios";
-import { useSearchParams } from "next/navigation";
-import React, { useCallback, Suspense } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout,
-} from "@stripe/react-stripe-js";
-
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-);
+import { Suspense } from "react";
+import CheckoutClient from "./CheckoutClient";
 
 function CheckoutPage() {
-  const searchParams = useSearchParams();
-  const orderId = searchParams.get("orderId");
-  const cartId = searchParams.get("cartId");
-
-  const fetchClientSecret = useCallback(async () => {
-    const response = await axios.post("/api/payment", {
-      orderId,
-      cartId,
-    });
-    return response.data.clientSecret;
-  }, [orderId, cartId]);
-
-  const options = { fetchClientSecret };
-
   return (
     <section>
       <div id="checkout">
         <Suspense fallback={<p>Loading checkout...</p>}>
-          <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-            <EmbeddedCheckout />
-          </EmbeddedCheckoutProvider>
+          <CheckoutClient />
         </Suspense>
       </div>
     </section>
