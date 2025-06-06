4733f9568e666dd0abe70f19a353fcc408be3031
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-06 10:04:10 +0300

    style: add some icons for special

--- data/profile/special.ts
+++ data/profile/special.ts
@@ -1,6 +1,7 @@
 import StrengthIcon from "@/public/images/special/strength.png";
 import EnduranceIcon from "@/public/images/special/endurance.png";
-// import StrengthIcon from "@/public/images/special/strength.png";
+import CharismaIcon from "@/public/images/special/charisma.png";
+import LuckIcon from "@/public/images/special/luck.png";
 
 export const specialData = [
   {
@@ -21,12 +22,12 @@ export const specialData = [
       "Your stamina and resilience. A higher rating suggests you’ll last longer on long shifts — or longer expeditions beyond the Vault.",
     icon: EnduranceIcon,
   },
-  //   {
-  //     name: "charisma",
-  //     description:
-  //       "Measures your social presence. Perfect for bartering, leading group activities, or persuading Overseers — if that’s your thing.",
-  //     icon: CharismaIcon,
-  //   },
+  {
+    name: "charisma",
+    description:
+      "Measures your social presence. Perfect for bartering, leading group activities, or persuading Overseers — if that’s your thing.",
+    icon: CharismaIcon,
+  },
   //   {
   //     name: "intelligence",
   //     description:
@@ -39,10 +40,10 @@ export const specialData = [
   //       "Your speed and flexibility. Great for quick tasks, navigating tight spaces, or dodging malfunctioning Protectrons.",
   //     icon: AgilityIcon,
   //   },
-  //   {
-  //     name: "luck",
-  //     description:
-  //       "A hard-to-define trait, but somehow things just go your way. Luck might mean winning at cards or finding the last can of Cram.",
-  //     icon: LuckIcon,
-  //   },
+  {
+    name: "luck",
+    description:
+      "A hard-to-define trait, but somehow things just go your way. Luck might mean winning at cards or finding the last can of Cram.",
+    icon: LuckIcon,
+  },
 ];
