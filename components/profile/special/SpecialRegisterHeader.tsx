fe17a15bff281b8e65df0db4ffbd6a2a230783fd
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-06 10:42:14 +0300

    fix: fix markup for special section

--- components/profile/special/SpecialRegisterHeader.tsx
+++ components/profile/special/SpecialRegisterHeader.tsx
@@ -1,20 +1,19 @@
 function SpecialRegisterHeader() {
   return (
-    <div>
-      <h2 className="md:text-5xl text-3xl -mt-6 text-center">
+    <div className="flex flex-col items-center text-center gap-2 px-4">
+      <h2 className="md:text-5xl text-3xl -mt-6">
         Welcome to Your S.P.E.C.I.A.L. Profile
       </h2>
-      <p className="text-base text-muted-foreground sm:text-lg md:text-xl lg:text-lg max-w-xl mx-auto mt-2 px-4 text-center">
+
+      <p className="text-base text-muted-foreground sm:text-lg md:text-xl lg:text-lg max-w-xl">
         Out here, everyone plays a part. Set your S.P.E.C.I.A.L. stats to unlock
         the G.O.A.T. Test and find your place in Vault 66’s trading network.
       </p>
 
-      <div className="flex items-center flex-col justify-center">
-        <h3 className="text center text-8xl">21</h3>
-        <p className="text-2xl uppercase text-muted-foreground">
-          points available
-        </p>
-      </div>
+      <h3 className="text-8xl">21</h3>
+      <p className="text-2xl uppercase text-muted-foreground">
+        points available
+      </p>
     </div>
   );
 }
