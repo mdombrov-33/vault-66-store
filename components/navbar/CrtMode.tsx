b71861c8271d3c3f379a0bc17653291e563de643
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-06 05:06:04 +0300

    fix: fix hydration bug with crt

--- components/navbar/CrtMode.tsx
+++ components/navbar/CrtMode.tsx
@@ -3,27 +3,35 @@
 import { useCrtMode } from "@/components/navbar/hooks/useCrtMode";
 import { Button } from "@/components/ui/button";
 import { TvIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 function CrtMode() {
   const { toggle, isEnabled, isDarkMode } = useCrtMode();
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
   if (!isDarkMode) return null;
 
-  return (
-    <Button
-      onClick={toggle}
-      size="icon"
-      variant="outline"
-      aria-pressed={isEnabled}
-      aria-label="Toggle CRT Mode"
-      className={
-        isEnabled ? (isDarkMode ? "crt-glow-dark" : "crt-glow-light") : ""
-      }
-    >
-      <span className="sr-only">CRT Mode Toggle</span>
-      <TvIcon />
-    </Button>
-  );
+  if (!mounted)
+    return (
+      <Button
+        onClick={toggle}
+        size="icon"
+        variant="outline"
+        aria-pressed={isEnabled}
+        aria-label="Toggle CRT Mode"
+        className={
+          isEnabled ? (isDarkMode ? "crt-glow-dark" : "crt-glow-light") : ""
+        }
+      >
+        <span className="sr-only">CRT Mode Toggle</span>
+        <TvIcon />
+      </Button>
+    );
 }
 
 export default CrtMode;
