399c97d1250e727352ec786844ce5848ffdf546f
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-06 18:46:01 +0300

    docs: remove logs

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,7 +6,11 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons";
 function Footer() {
   return (
     <footer className="flex">
-      <Button asChild className="fixed bottom-5 left-5" size={"icon"}>
+      <Button
+        asChild
+        className="fixed bottom-5 left-5 hidden 2xl:block"
+        size={"icon"}
+      >
         <Link href="https://github.com/mdombrov-33/vault-66-store">
           <GitHubLogoIcon />
           <span className="sr-only">Open GitHub repository</span>
--- components/navbar/CrtMode.tsx
+++ components/navbar/CrtMode.tsx
@@ -8,8 +8,6 @@ import { useGlowClass } from "./hooks/useGlowClass";
 
 function CrtMode() {
   const { toggle, isEnabled, resolvedTheme } = useCrtMode();
-  console.log("isEnabled:", isEnabled);
-  console.log("resolvedTheme:", resolvedTheme);
 
   const glowClass = useGlowClass(isEnabled);
 
