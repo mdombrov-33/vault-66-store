643b3dc1758591f361d78d16a0462cccffeb1526
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-11 16:26:41 +0300

    fix goatintro

--- components/profile/special/results/SpecialPageResults.tsx
+++ components/profile/special/results/SpecialPageResults.tsx
@@ -4,7 +4,7 @@ import { SpecialPageResultsProps } from "@/types/profile";
 
 function SpecialPageResults({ specialRecord }: SpecialPageResultsProps) {
   return (
-    <section className="max-w-5xl mx-auto px-2 grid grid-cols-1 md:grid-cols-2 gap-8 -mt-6 pb-26 sm:pb-16">
+    <section className="max-w-5xl mx-auto px-2 grid grid-cols-1 md:grid-cols-2 gap-8 -mt-6 pb-26 sm:pb-0">
       {/* Left Column: SPECIAL Results */}
       <div className="bg-primary/10 rounded-lg shadow-md flex flex-col items-center justify-center ">
         <h1 className="text-3xl font-bold uppercase mb-4 tracking-wider py-6 px-4">
