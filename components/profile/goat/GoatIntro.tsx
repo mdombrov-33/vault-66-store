8f07a2ed91c67ce30f89c79b2fdd98d6bfd3c06c
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-11 16:01:36 +0300

    fix goatintro

--- components/profile/goat/GoatIntro.tsx
+++ components/profile/goat/GoatIntro.tsx
@@ -7,13 +7,17 @@ function GoatIntro({ handleStart }: GoatIntroProps) {
   const { playClick } = useSoundPlayer();
 
   return (
-    <section className="flex flex-col items-center justify-center text-center gap-6 pb-4">
+    <section
+      className="flex flex-col items-center justify-center text-center gap-6 pb-16 md:pb-12
+"
+    >
       <Image
-        width={400}
-        height={400}
         src="/images/goat/welcome.png"
         alt="Vault 66 Welcome"
-        className="shadow-lg rounded-lg border border-muted"
+        width={400}
+        height={400}
+        className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg shadow-lg rounded-lg border border-muted"
+        sizes="(max-width: 640px) 90vw, (max-width: 1024px) 50vw, 400px"
         priority
       />
       <h2 className="md:text-6xl text-5xl">Welcome to Vault 66</h2>
