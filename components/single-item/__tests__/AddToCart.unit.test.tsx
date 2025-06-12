d0641b476b76ed028e062d2f10a0567e752a0c58
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-12 21:26:49 +0300

    test: create unit for addtocart

---
+++ components/single-item/__tests__/AddToCart.unit.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import AddToCart from "../AddToCart";
+import { useAuth } from "@clerk/nextjs";
+
+const playClickMock = vi.fn();
+
+vi.mock("@/components/form/FormContainer", () => ({
+  default: ({ children }: any) => <form>{children}</form>,
+}));
+
+vi.mock("@clerk/nextjs", () => ({
+  useAuth: vi.fn(),
+  SignInButton: ({ children }: { children: React.ReactNode }) => (
+    <>{children}</>
+  ),
+}));
+
+vi.mock("@/hooks/useSoundPlayer", () => ({
+  useSoundPlayer: () => ({
+    playClick: playClickMock,
+  }),
+}));
+
+describe("AddToCart component", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("renders sign in button when user is not authenticated", () => {
+    (useAuth as ReturnType<typeof vi.fn>).mockReturnValue({ userId: null });
+
+    render(<AddToCart productId="123" />);
+    const signInButton = screen.getByRole("button", { name: /sign in/i });
+    expect(signInButton).toBeInTheDocument();
+  });
+
+  it("renders add to supply bin button when user is authenticated", () => {
+    (useAuth as ReturnType<typeof vi.fn>).mockReturnValue({
+      userId: "user123",
+    });
+
+    render(<AddToCart productId="123" />);
+    const addToSupplyBinButton = screen.getByRole("button", {
+      name: /add to supply bin/i,
+    });
+    expect(addToSupplyBinButton).toBeInTheDocument();
+  });
+
+  it("calls playClick when add to supply bin button is clicked", async () => {
+    const user = userEvent.setup();
+    (useAuth as ReturnType<typeof vi.fn>).mockReturnValue({
+      userId: "user123",
+    });
+
+    render(<AddToCart productId="123" />);
+    const addToSupplyBinButton = screen.getByRole("button", {
+      name: /add to supply bin/i,
+    });
+
+    await user.click(addToSupplyBinButton);
+    expect(playClickMock).toHaveBeenCalledTimes(1);
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 //* environment: 'jsdom' – Simulates a browser (needed for DOM testing)
 //* setupFiles – This runs before every test file
 //* globals: true – so we don’t have to import describe, test, etc. every time
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: "./setupTests.ts",
