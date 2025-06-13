d6929bc3b39addebd1ab8523b05a1dbfa7f62445
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-13 22:00:29 +0300

    config: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,17 @@ const eslintConfig = [
     ignores: ['lib/generated/prisma', 'node_modules', 'dist', '.next'],
   },
 
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends('next/core-web-vitals', 'plugin:prettier/recommended'),
+
+  {
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      'next/next/no-img-element': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
 ]
 
 export default eslintConfig
