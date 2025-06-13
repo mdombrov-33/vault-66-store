f29ef817836d4992225216db7a060e9f3ec59166
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-13 22:06:21 +0300

    config: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,9 +17,6 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'plugin:prettier/recommended'),
 
   {
-    plugins: {
-      prettier: {},
-    },
     rules: {
       'prettier/prettier': 'error',
       'no-console': 'warn',
