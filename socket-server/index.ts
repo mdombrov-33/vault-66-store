7562559a8797677dc6d643307af3217a44839d80
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-17 22:14:11 +0300

    feat: setup basic socket server

---
+++ socket-server/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import http from 'http'
+
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log(`User connected:,${socket.id} `)
+
+  socket.on('chat message', (msg: string) => {
+    console.log(`Message received: ${msg}`)
+    io.emit('chat message', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`)
+  })
+})
+
+const PORT = 4000
+server.listen(PORT, () => {
+  console.log(`Socket server is running on http://localhost:${PORT}`)
+})
