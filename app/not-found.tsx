9292a19f6bedbae425487f62b04cd57fd1e1a084
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-18 14:51:45 +0300

    style: update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,44 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+'use client'
 
-function NotFound() {
+import { Button } from '@/components/ui/button'
+import { useSoundPlayer } from '@/hooks/useSoundPlayer'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+export default function NotFoundPage() {
+  const router = useRouter()
+  const { playClick } = useSoundPlayer()
+
   return (
-    <div className="flex flex-col items-center justify-center  text-center p-8">
-      <h1 className="text-4xl font-bold mb-4">404 – Page Not Found</h1>
-      <p className="text-lg mb-6">Sorry, we couldn’t find the page you’re looking for.</p>
-      <Button asChild variant="default">
-        <Link href="/" className="hover:underline">
-          Go back home
-        </Link>
+    <div className="flex flex-col items-center justify-center px-4 py-12 -mt-20  text-center">
+      <Image
+        src="/images/not-found.png"
+        alt="Vault Boy searching with a magnifying glass"
+        width={280}
+        height={280}
+        className="mb-6"
+        priority
+      />
+      <h1 className="text-5xl font-extrabold text-primary mb-2">Error 404: Location Not Found</h1>
+      <p className="text-lg text-muted-foreground max-w-lg mx-auto mb-6 leading-relaxed">
+        Looks like even Vault Boy can’t find this place.
+        <br />
+        Maybe it’s a hidden vault, or the wasteland swallowed it whole.
+        <br />
+        Try heading back to safer ground.
+      </p>
+      <Button
+        size="lg"
+        variant="default"
+        className="bg-primary text-primary-foreground text-2xl"
+        onClick={() => {
+          playClick()
+          router.push('/')
+        }}
+        aria-label="Go back to homepage"
+      >
+        Return to Vault
       </Button>
     </div>
   )
 }
-
-export default NotFound
