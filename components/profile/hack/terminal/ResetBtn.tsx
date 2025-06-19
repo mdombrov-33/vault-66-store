0cb7f34f361eac1c011365f108b226057f60e202
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-20 02:39:54 +0300

    style/fix: fix reset button color in hacking

--- components/profile/hack/terminal/ResetBtn.tsx
+++ components/profile/hack/terminal/ResetBtn.tsx
@@ -4,12 +4,13 @@ import { ResetBtnProps } from '@/types/profile'
 function ResetBtn({ resetGame, setOnWordHover }: ResetBtnProps) {
   return (
     <Button
+      variant="default"
       onClick={() => {
         resetGame()
         setOnWordHover(null)
       }}
       size="sm"
-      className="px-6 py-2  bg-[var(--terminal-text)] text-black rounded hover:bg-primary/40 md:text-xl"
+      className="px-6 py-2  bg-[var(--terminal-text)] text-black rounded md:text-xl"
     >
       RETRY
     </Button>
