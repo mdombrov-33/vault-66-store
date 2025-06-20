7e20872023bd00b8cfd3aefb138318794f9c30e1
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-20 18:59:29 +0300

    fix/style(hero): improve hero section - tweak sizes, font, mobile view

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -11,9 +11,11 @@ function Hero() {
 
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-      <div>
-        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-5xl uppercase">
-          Welcome to Vault 66 — Your Post-Apocalyptic Marketplace
+      <div className="flex flex-col items-center justify-center lg:items-start lg:justify-start">
+        <h1 className="max-w-2xl font-medium text-4xl tracking-tight text-center lg:text-start sm:text-5xl  uppercase">
+          Welcome to Vault 66
+          <br />
+          Your Post-Apocalyptic Marketplace
         </h1>
 
         <TypeAnimation
@@ -28,7 +30,7 @@ function Hero() {
             5000,
           ]}
           wrapper="p"
-          className="mt-8 min-h-16 max-w-xl text-2xl leading-8 text-muted-foreground uppercase"
+          className="mt-8 min-h-16 max-w-xl md:text-2xl text-xl leading-8 text-muted-foreground uppercase"
           repeat={Infinity}
           speed={90}
           cursor={true}
