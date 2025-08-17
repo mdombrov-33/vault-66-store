960124cae4d92cdfdf3e337cfa7e9d94ef3123b5
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-17 21:50:13 +0300

    fix(reviews): fix absent typing sound when writing review for product

--- components/form/TextAreaInput.tsx
+++ components/form/TextAreaInput.tsx
@@ -1,8 +1,11 @@
 import { Label } from '../ui/label'
 import { Textarea } from '../ui/textarea'
 import { TextAreaInputProps } from '@/types/form'
+import { useTypingSounds } from '@/hooks/useTypingSounds'
 
 function TextAreaInput({ name, labelText, defaultValue }: TextAreaInputProps) {
+  const { playTypingSound } = useTypingSounds()
+
   return (
     <div className="mb-2">
       <Label htmlFor={name} className="capitalize text-xl">
@@ -15,6 +18,7 @@ function TextAreaInput({ name, labelText, defaultValue }: TextAreaInputProps) {
         required
         className="leading-loose text-xl font-[roboto]"
         defaultValue={defaultValue}
+        onChange={playTypingSound}
       />
     </div>
   )
